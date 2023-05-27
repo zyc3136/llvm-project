bba33c3aa488a36862283576aa7f90f144770d17
DESKTOP-JF2O2IM\admin <15642355801@163.com>
2023-05-27 23:54:02 +0800

    no message

--- llvm/lib/Transforms/Obfuscation/HelloWorld.cpp
+++ llvm/lib/Transforms/Obfuscation/HelloWorld.cpp
@@ -2,14 +2,12 @@
 #include "llvm/IR/LegacyPassManager.h"
 #include "llvm/Support/raw_ostream.h"
 #include "llvm/Transforms/IPO/PassManagerBuilder.h"
-#include "llvm/Transforms/Obfuscation/Utils.h"
 #include <iostream>
 using namespace llvm;
 
 bool HelloWorld::runOnFunction(Function &F) {
     if (enable) {
-        SKIP_IF_SHOULD(F);
-        outs() << "Hello, " << F.getName() << ", " << readAnnotation(&F) << "\n";
+        outs() << "Hello, " << F.getName() << "\n";
     }
     return false;
 }
